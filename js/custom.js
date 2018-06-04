f4f81b41692a2215ff4ae9a9f5c016ce0346c7ed
coddec <jimdoxin@gmail.com>
2018-06-04 21:50:32 +1000

    Add scroll to top/bottom button

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+	$(document).ready(function () {
+		$(window).scroll(function () {
+			//Back to top
+			if ($(this).scrollTop() > 0) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+
+			//Back top bottom
+			if ($(window).scrollTop() + $(window).height() !== $(document).height()) {
+				$('#back-to-bottom').fadeIn();
+			} else {
+				$('#back-to-bottom').fadeOut();
+			}
+		});
+
+		// scroll body to bottom on click
+		$('#back-to-bottom').click(function () {
+			$('#back-to-bottom').tooltip('hide');
+			$('body,html').animate({
+				scrollTop: $(document).height()
+			}, 500);
+			return false;
+		});
+
+		$('#back-to-bottom').tooltip();
+
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('#back-to-top').tooltip('hide');
+			$('body,html').animate({
+				scrollTop: 0
+			}, 500);
+			return false;
+		});
+
+		$('#back-to-top').tooltip();
+
+	});
